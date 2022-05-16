96c2426f7caf1d460ee1e57d44988268a991841d
TranTuanKiet0110 <s3879300@rmit.edu.vn>
2022-05-16 17:09:24 +0700

    create function to calculate house-rating score

--- house.cpp
+++ house.cpp
@@ -2,5 +2,29 @@
 using namespace std;
 
 class House {
+    private:
 
+    public:
+        string location;
+        string description;
+        float scoreRating[];
+
+        House(){
+
+        }
+
+        House(string location, string description, float scoreRating[]){
+            this->location = location;
+            this->description = description;
+            for(int i = 0; i < sizeof(scoreRating); i++){
+                this->scoreRating[i] = scoreRating[i];
+            }
+        }
+        
+        void houseRatingScore (float scoreRating[]){
+            float sum = 0.0;
+            for(int i = 0; i < sizeof(scoreRating); i++){
+                sum += scoreRating[i];
+            }
+        }
 };
