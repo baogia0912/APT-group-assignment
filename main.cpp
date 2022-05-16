74ecfb7cd46de4a9e5186a2093244959d188f01f
Bao <bao@Baos-MBP-2.STUSGS.RMIT.EDU.VN>
2022-05-16 14:38:48 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "menu.cpp"
+using namespace std;
+
+int main() {
+    menu();
+}
---
+++ menu.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void menu() {
+    cout << "This is the menu" << endl;
+}
