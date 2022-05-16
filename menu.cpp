92f3b6f53a06a2165e0c5ae25f0f3b1fd5892744
Bao <bao@Baos-MBP-2.STUSGS.RMIT.EDU.VN>
2022-05-16 15:57:11 +0700

    doing menu

--- menu.cpp
+++ menu.cpp
@@ -3,5 +3,23 @@
 using namespace std;
 
 void menu() {
+    int option;
+    string username, password;
+
     cout << "This is the menu" << endl;
+    cin >> option;
+
+    switch (option) {
+        case 1: 
+            cout << "Enter username: ";
+            cin >> username;
+            cout << "Enter password: ";
+            cin >> password;
+
+            if (logIn(username, password)) {
+
+            }
+            
+        case 2: logInAsGuest();
+    }
 }
